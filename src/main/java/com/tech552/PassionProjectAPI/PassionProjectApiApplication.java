a363be94fecade3dda6a5a1e7c2814298b045653
mikailaAkeredolu <mikaila@tech552.com>
2024-06-26 13:33:01 -0400

    ready for your passion

--- src/main/java/com/tech552/PassionProjectAPI/PassionProjectApiApplication.java
+++ src/main/java/com/tech552/PassionProjectAPI/PassionProjectApiApplication.java
@@ -14,12 +14,6 @@ public class PassionProjectApiApplication {
 	}
 
 
-	/*
-	we must create a method that builds (through a RestTemplateBuilder) and returns a RestTemplate and mark it with @Bean Spring annotation.
-	 As the documentation points out, this annotation is used to indicate that a method produces a bean to be managed by the Spring container.
-
-	 */
-
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
